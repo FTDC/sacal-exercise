1ec790bed638551f0892671015237cee551e3ac4
susan <susan@live.cn>
2021-01-05 11:09:45 +0800

    测试文件

---
+++ src/chapter01/test.scala
@@ -0,0 +1,23 @@
+package com.xjin.demo
+package chapter01
+
+object test {
+  def main(args: Array[String]): Unit = {
+
+    val age = 22.0 / 2
+
+
+    for (i <- 1 to 12) {
+      print(i + "\n")
+    }
+
+
+    for (i <- 1 to(20, 2)) {
+      print(i + "\n")
+    }
+
+    print("name" + age)
+
+
+  }
+}
---
+++ src/chapter02/change01.scala
@@ -0,0 +1,19 @@
+package com.xjin.demo
+package chapter02
+
+object change01 {
+  def main(args: Array[String]): Unit = {
+    var a = 10
+    var b = 20
+    a = a + b
+    b = a - b
+    a = a - b
+    print(a + "\n")
+    print(b)
+
+    //  位运算
+
+
+  }
+
+}
